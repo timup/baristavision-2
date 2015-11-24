7666c72044366b2d2bda616bda9705d02685f0c4
Tim Upchurch <mail@timup.ch>
2015-11-24 15:15:20 -0500

    add item create_from_api class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,17 +8,26 @@ class Item < ActiveRecord::Base
 
   def self.sync_with_api items
   	active_item_ids = []
+
   	for item in items
-  		self.find_or_create_by!(item_id: item['id']) do |i| 
-  			i.name = item['name']
-  		end
+  		create_from_api(item)
   		active_item_ids << item['id']
   	end
+
   	user = Item.find_by(item_id: active_item_ids.last).user
   	user_item_ids = user.items.map { |item| item.item_id }
+
+    archive_item_ids = user_item_ids - active_item_ids
+    for item_id in archive_item_ids
+      item = Item.find_by(item_id: item_id)
+      # create class method to toggle 
+      # item.soft_delete
+    end
   end
 
   def self.create_from_api item
+    self.find_or_create_by!(item_id: item['id']) do |i|
+      i.name = item['name']
   end
 
 end
